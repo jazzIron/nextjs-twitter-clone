3a42286e72ae14853a2897a8c04d91b7c4e3f790
iron <dlwocjf90@gmail.com>
2022-10-23 20:15:11 +0900

    add animation effect using framer motion

--- components/Feed.js
+++ components/Feed.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -28,9 +29,19 @@ function Feed() {
         </div>
       </div>
       <Input />
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post key={post.id} post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
