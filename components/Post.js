7ac43e25f11e33143993b78c8271826d969be71e
iron <dlwocjf90@gmail.com>
2022-10-23 21:22:01 +0900

    install and implement react-modal

--- components/Post.js
+++ components/Post.js
@@ -20,13 +20,14 @@ import { db, storage } from "../firebase";
 import { useEffect, useState } from "react";
 import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atom/modalAtom";
+import { modalState, postIdState } from "../atom/modalAtom";
 
 function Post({ post }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -109,7 +110,14 @@ function Post({ post }) {
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2">
           <ChatBubbleLeftEllipsisIcon
-            onClick={() => setOpen(!open)}
+            onClick={() => {
+              if (!session) {
+                signIn();
+              } else {
+                setPostId(post.id);
+                setOpen(!open);
+              }
+            }}
             className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
           />
           {session?.user.uid === post?.data().id && (
